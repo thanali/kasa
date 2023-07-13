753b15bd46bf02194f8ffcf6cc3bc393a72abe86
lithana <lbn.nathalie@gmail.com>
2023-07-13 18:42:14 +0200

    Add About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,17 @@
 import Banner from "../components/Banner"
 import Dropdown from "../components/Dropdown"
 import mountains from "../assets/images/mountains.png"
+import { aboutListContent as dataAbout } from "../data/aboutListContent"
 
 function About() {
   return (
     <>
       <Banner image={mountains} title={"Mountains"} />
-      <Dropdown />
+      <section className="dropdown-container">
+        {dataAbout.map(({ title, content }) => (
+          <Dropdown key={title} title={title} content={content} />
+        ))}
+      </section>
     </>
   )
 }
