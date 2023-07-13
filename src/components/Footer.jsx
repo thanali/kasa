737ae1eed5946cba530b7b8be0a40e61a051b0d7
lithana <lbn.nathalie@gmail.com>
2023-07-13 15:51:30 +0200

    Add Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/logo/logo.png"
+
+function Footer() {
+  return (
+    <footer>
+      <img className="footer-logo" src={logo} alt="Logo Kasa" />
+      <p>©2020 Kasa. All rights reserved</p>
+    </footer>
+  )
+}
+
+export default Footer
