6f81069095c49fe5b4573e6cd381f4cfc32098b2
lithana <lbn.nathalie@gmail.com>
2023-08-02 13:35:41 +0200

    Add verification data in Accomodation Component

--- src/pages/Accomodation.jsx
+++ src/pages/Accomodation.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom"
 import Dropdown from "../components/Dropdown"
 import emptyStar from "../assets/icons/star-empty.svg"
 import fullStar from "../assets/icons/star-active.svg"
+import ErrorPage from "./ErrorPage"
 
 function Accomodation() {
   // Renvoie automatique en haut de page au chargement
@@ -13,73 +14,80 @@ function Accomodation() {
   const { id } = useParams()
   // Le compare avec les données
   const data = db && db.find(el => el.id === id)
-  // Récupère les props du paramètre qui correspond
-  const {
-    title,
-    pictures,
-    location,
-    tags,
-    description,
-    host,
-    rating,
-    equipments
-  } = data
 
-  // Recherche les espaces, les retire et créé un nouveau tableau d'éléments
-  const nameTag = host.name.split(" ")
+  // Consition si les id correspondent
+  if (data) {
+    // Récupère les props du paramètre qui correspond
+    const {
+      title,
+      pictures,
+      location,
+      tags,
+      description,
+      host,
+      rating,
+      equipments
+    } = data
+    // Recherche les espaces, les retire et créé un nouveau tableau d'éléments
+    const nameTag = host.name.split(" ")
 
-  return (
-    <section className="accomodation">
-      <Slider imgList={pictures} title={title} />
+    return (
+      <section className="accomodation">
+        <Slider imgList={pictures} title={title} />
 
-      <div className="accomodation-content">
-        <div className="acc-text">
-          <h1 className="acc-text-title">{title}</h1>
-          <p className="acc-text-location">{location}</p>
+        <div className="accomodation-content">
+          <div className="acc-text">
+            <h1 className="acc-text-title">{title}</h1>
+            <p className="acc-text-location">{location}</p>
 
-          <div className="tags">
-            {tags &&
-              tags.map((content, index) => (
-                <div key={`${index}-tag`} className="tag">
-                  <h3>{content}</h3>
-                </div>
-              ))}
-          </div>
-        </div>
-
-        <div className="host-rating">
-          <div className="host">
-            <img src={host.picture} alt={host.name} />
-            <div className="host-name">
-              {nameTag.map(el => (
-                <p key={el}>{el}</p>
-              ))}
+            <div className="tags">
+              {tags &&
+                tags.map((content, index) => (
+                  <div key={`${index}-tag`} className="tag">
+                    <h3>{content}</h3>
+                  </div>
+                ))}
             </div>
           </div>
 
-          <div className="rating">
-            {[...Array(5)].map((star, index) => (
-              <img
-                key={`${index}-star`}
-                src={rating < index + 1 ? emptyStar : fullStar}
-                alt={rating < index + 1 ? "Étoile vide" : "Étoile sélectionnée"}
-              />
-            ))}
+          <div className="host-rating">
+            <div className="host">
+              <img src={host.picture} alt={host.name} />
+              <div className="host-name">
+                {nameTag.map(el => (
+                  <p key={el}>{el}</p>
+                ))}
+              </div>
+            </div>
+
+            <div className="rating">
+              {[...Array(5)].map((star, index) => (
+                <img
+                  key={`${index}-star`}
+                  src={rating < index + 1 ? emptyStar : fullStar}
+                  alt={
+                    rating < index + 1 ? "Étoile vide" : "Étoile sélectionnée"
+                  }
+                />
+              ))}
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="accomodation-dropdowns">
-        <Dropdown title={"Description"} content={description} />
-        <Dropdown
-          title={"Équipements"}
-          content={equipments.map((el, index) => (
-            <li key={`${index}-equipment`}>{el}</li>
-          ))}
-        />
-      </div>
-    </section>
-  )
+        <div className="accomodation-dropdowns">
+          <Dropdown title={"Description"} content={description} />
+          <Dropdown
+            title={"Équipements"}
+            content={equipments.map((el, index) => (
+              <li key={`${index}-equipment`}>{el}</li>
+            ))}
+          />
+        </div>
+      </section>
+    )
+  } else {
+  }
+  return <ErrorPage />
 }
 
 export default Accomodation
